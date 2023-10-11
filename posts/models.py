70a48adf72f030bfc118983263cb4014ddc18501
Otunla Timilehin <69770115+Duade10@users.noreply.github.com>
2023-10-11 23:25:27 +0100

    created post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from core.models import AbstractTimestampFields
+from categories.models import Category
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(AbstractTimestampFields):
+    title = models.CharField(max_length=1000)
+    slug = models.SlugField(blank=True, null=True)
+    image = models.ImageField(upload_to="blog/photos")
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
