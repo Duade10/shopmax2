f2bff9822121d421f958e839006c23f4f55494aa
Otunla Timilehin <69770115+Duade10@users.noreply.github.com>
2023-10-11 23:00:06 +0100

    created categories model

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.text import slugify
+from core.models import AbstractTimestampFields
+
+
+class Category(AbstractTimestampFields):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
